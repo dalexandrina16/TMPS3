eec7eff57c330fddc7c3b502083dbc17c257df2a
Dina-Alexandrina Bizgu <dina.bizgu.99@gmail.com>
2021-01-30 13:09:05 +0200

    first commit

---
+++ behavioural-dp/Strategy/Strategy.java
@@ -0,0 +1,13 @@
+class Strategy
+{
+    public static void main(String args[])
+    {
+        Transformer t = new Transformer();
+        t.setJump(new SuperJump());
+        t.setKick(new SuperKick());
+        t.performAction();
+    }
+}
+
+/* This example has a transformer that can perform 3 actions kick,jump and roll; each action has 2 types basic and super; so created a class
+separated for each behaviour and then setting the transformer properties with these objects at runtime; and then letting it perfrom action */
